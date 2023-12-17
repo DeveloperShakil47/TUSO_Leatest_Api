3fbe2539368713866e8b3308309cf3123c81c528
Shakil <shakilisdb47@gmail.com>
2023-12-17 12:49:56 +0600

    Repository add by shakil

---
+++ TUSO.Infrastructure.SqlServer/DataContext.cs
@@ -0,0 +1,187 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TUSO.Domain.Entities;
+
+/*
+ * Created by: Stephan
+ * Date created: 17.12.2023
+ * Last modified:
+ * Modified by: 
+ */
+namespace TUSO.Infrastructure.SqlServer
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        /// <summary>
+        /// Represents Countries entity.
+        /// </summary>
+        public DbSet<Country> Countries { get; set; }
+
+        /// <summary>
+        /// Represents Provinces entity.
+        /// </summary>
+        public DbSet<Province> Provinces { get; set; }
+
+        /// <summary>
+        /// Represents Districts entity.
+        /// </summary>
+        public DbSet<District> Districts { get; set; }
+
+        /// <summary>
+        /// Represents Facilities entity.
+        /// </summary>
+        public DbSet<Facility> Facilities { get; set; }
+
+        /// <summary>
+        /// Represents FacilityPermissions entity.
+        /// </summary>
+        public DbSet<FacilityPermission> FacilityPermissions { get; set; }
+
+        /// <summary>
+        /// Represents Roles entity.
+        /// </summary>
+        public DbSet<Role> Roles { get; set; }
+
+        /// <summary>
+        /// Represents UserAccounts entity.
+        /// </summary>
+        public DbSet<UserAccount> UserAccounts { get; set; }
+
+        /// <summary>
+        /// Represents UserAccounts entity.
+        /// </summary>
+        public DbSet<DeviceType> DeviceTypes { get; set; }
+
+        /// <summary>
+        /// Represents ProfilePicture entity.
+        /// </summary>
+        public DbSet<ProfilePicture> ProfilePictures { get; set; }
+
+        /// <summary>
+        /// Represents Modules entity.
+        /// </summary>
+        public DbSet<Module> Modules { get; set; }
+
+        /// <summary>
+        /// Represents ModulePermissions entity.
+        /// </summary>
+        public DbSet<ModulePermission> ModulePermissions { get; set; }
+
+        /// <summary>
+        /// Represents Incidents entity.
+        /// </summary>
+        public DbSet<Incident> Incidents { get; set; }
+
+        /// <summary>
+        /// Represents IncidentActionLog entity.
+        /// </summary>
+        public DbSet<IncidentActionLog> IncidentActionLogs { get; set; }
+
+        /// <summary>
+        /// Represents IncidentAdminActionLog entity.
+        /// </summary>
+        public DbSet<IncidentAdminActionLog> IncidentAdminActionLogs { get; set; }
+
+        /// <summary>
+        /// Represents IncidentCategories entity.
+        /// </summary>
+        public DbSet<IncidentCategory> IncidentCategories { get; set; }
+
+        /// <summary>
+        /// Represents IncidentPriorities entity.
+        /// </summary>
+        public DbSet<IncidentPriority> IncidentPriorities { get; set; }
+
+        /// <summary>
+        /// Represents Members entity.
+        /// </summary>
+        public DbSet<Member> Members { get; set; }
+
+        /// <summary>
+        /// Represents Messages entity.
+        /// </summary>
+        public DbSet<Message> Messages { get; set; }
+
+        /// <summary>
+        /// Represents Screenshots entity.
+        /// </summary>
+        public DbSet<Screenshot> Screenshots { get; set; }
+
+        /// <summary>
+        /// Represents Projects entity.
+        /// </summary>
+        public DbSet<Project> Projects { get; set; }
+
+        /// <summary>
+        /// Represents SystemPermission entity.
+        /// </summary>
+        public DbSet<SystemPermission> SystemPermissions { get; set; }
+
+        /// <summary>
+        /// Represents Projects entity.
+        /// </summary>
+        public DbSet<Team> Teams { get; set; }
+
+        /// <summary>
+        /// Represents RecoveryRequests entity.
+        /// </summary>
+        public DbSet<RecoveryRequest> RecoveryRequests { get; set; }
+
+        /// <summary>
+        /// Represents FundingAgency entity.
+        /// </summary>
+        public DbSet<FundingAgency> FundingAgencies { get; set; }
+
+        /// <summary>
+        /// Represents Implementing Partners entity.
+        /// </summary>
+        public DbSet<ImplementingPartner> ImplementingPartners { get; set; }
+
+
+        /// <summary>
+        /// Represents RdpServerInfo entity.
+        /// </summary>
+        public DbSet<RdpServerInfo> RdpServerInfos { get; set; }
+
+        /// <summary>
+        /// Represents RDPDeviceInfo entity.
+        /// </summary>
+        public DbSet<RDPDeviceInfo> RDPDeviceInfoes { get; set; }
+
+        /// <summary>
+        /// Represents EmailConfigurations entity.
+        /// </summary>
+        public DbSet<EmailConfiguration> EmailConfigurations { get; set; }
+
+        /// <summary>
+        /// Represents Syncs entity.
+        /// </summary>
+        public DbSet<DeviceControl> DeviceControls  { get; set; }
+
+        /// <summary>
+        /// Represents EmailTemplates entity.
+        /// </summary>
+        public DbSet<EmailTemplate> EmailTemplates { get; set; }
+
+        /// <summary>
+        /// All Configuration Related Entity
+        /// </summary>
+        public DbSet<EmailControl> EmailControls  { get; set; }
+
+        /// <summary>
+        /// Represents RemoteLoginConcents entity.
+        /// </summary>
+        public DbSet<RemoteLoginConcent> RemoteLoginConcents { get; set; }
+    }
+}
---
+++ TUSO.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,248 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TUSO.Infrastructure.Contracts;
+using TUSO.Infrastructure.SqlServer;
+
+/*
+ * Created by: Stephan
+ * Date created: 17.12.2023
+ * Last modified:
+ * Modified by: 
+ */
+namespace TUSO.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Implementation of IRepository interface.
+    /// </summary>
+    /// <typeparam name="T">T is a model class.</typeparam>
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DataContext context;
+
+        public Repository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public virtual T Add(T entity)
+        {
+            try
+            {
+                return context.Set<T>().Add(entity).Entity;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Add multi object to database
+        /// </summary>
+        /// <param name="entities"></param>
+        public void AddRange(IEnumerable<T> entities)
+        {
+            context.Set<T>().AddRange(entities);
+        }
+        public virtual T Get(Guid id)
+        {
+            try
+            {
+                return context.Set<T>().Find(id);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public virtual T Get(int id)
+        {
+            try
+            {
+                return context.Set<T>().Find(id);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            try
+            {
+                return context.Set<T>()
+                    .AsQueryable()
+                    .AsNoTracking()
+                    .ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public virtual async Task<IEnumerable<T>> QueryAsync(Expression<Func<T, bool>> predicate, Expression<Func<T, object>> order)
+        {
+            try
+            {
+                return await context.Set<T>()
+                    .AsQueryable()
+                    .AsNoTracking()
+                    .Where(predicate)
+                    .OrderByDescending(order)
+                    .ToListAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Returns matched rows as a list of objects.
+        /// </summary>
+        /// <param name="predicate">Custom LINQ expression.</param>
+        /// <returns>List of objects.</returns>
+        public async Task<IEnumerable<T>> QueryAsync(Expression<Func<T, bool>> predicate)
+        {
+            try
+            {
+                return await context.Set<T>()
+                    .AsQueryable()
+                    .AsNoTracking()
+                    .Where(predicate)
+                    .ToListAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public virtual async Task<IEnumerable<T>> QueryAsync(Expression<Func<T, bool>> predicate, Expression<Func<T, object>> order, Expression<Func<T, object>> obj)
+        {
+            try
+            {
+                return await context.Set<T>()
+                    .AsQueryable()
+                    .Where(predicate)
+                    .OrderByDescending(order)
+                    .Include(obj)
+                    .ToListAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public virtual async Task<IEnumerable<T>> QueryAsync(Expression<Func<T, bool>> predicate, Expression<Func<T, object>> order, Expression<Func<T, object>> obj, Expression<Func<T, object>> next)
+        {
+            try
+            {
+                return await context.Set<T>()
+                    .AsQueryable()
+                    .Where(predicate)
+                    .OrderByDescending(order)
+                    .Include(obj)
+                    .Include(next)
+                    .ToListAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public virtual async Task<IEnumerable<T>> QueryAsync(Expression<Func<T, bool>> predicate, Expression<Func<T, object>> order, Expression<Func<T, object>> obj, Expression<Func<T, object>> next, Expression<Func<T, object>> obj2)
+        {
+            try
+            {
+                return await context.Set<T>()
+                      .AsQueryable()
+                      .Where(predicate)
+                      .OrderByDescending(order)
+                      .Include(obj)
+                      .Include(next)
+                      .Include(obj2)
+                      .ToListAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate)
+        {
+            try
+            {
+                return await context.Set<T>()
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(predicate);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate, Expression<Func<T, object>> obj)
+        {
+            try
+            {
+                return await context.Set<T>()
+                    .AsNoTracking()
+                    .Include(obj)
+                    .FirstOrDefaultAsync(predicate);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate, Expression<Func<T, object>> obj, Expression<Func<T, object>> next)
+        {
+            try
+            {
+                return await context.Set<T>()
+                    .AsNoTracking()
+                    .Include(obj)
+                    .Include(next)
+                    .FirstOrDefaultAsync(predicate);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public virtual void Update(T entity)
+        {
+            try
+            {
+                context.Entry(entity).State = EntityState.Modified;
+                context.Set<T>().Update(entity);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public virtual void Delete(T entity)
+        {
+            try
+            {
+                context.Set<T>().Remove(entity);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
