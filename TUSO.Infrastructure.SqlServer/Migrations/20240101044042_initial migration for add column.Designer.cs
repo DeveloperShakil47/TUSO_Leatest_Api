30e6d9f0921aa0833aee066991970acee531addf
Shakil <shakilisdb47@gmail.com>
2024-01-01 10:42:09 +0600

    sdf

--- TUSO.Infrastructure.SqlServer/Migrations/20240101044042_initial migration for add column.Designer.cs
+++ TUSO.Infrastructure.SqlServer/Migrations/20240101044042_initial migration for add column.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TUSO.Infrastructure.SqlServer;
 
@@ -11,9 +12,11 @@ using TUSO.Infrastructure.SqlServer;
 namespace TUSO.Infrastructure.SqlServer.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240101044042_initial migration for add column")]
+    partial class initialmigrationforaddcolumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TUSO.Infrastructure.SqlServer/Migrations/20240101044042_initial migration for add column.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TUSO.Infrastructure.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigrationforaddcolumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsITExpart",
+                table: "FacilityPermissions",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsITExpart",
+                table: "FacilityPermissions");
+        }
+    }
+}
